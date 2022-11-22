8b7c49e97a943d5352fcb258f26d287f8373818c
Sitipatimaah <118707249+Sitipatimaah@users.noreply.github.com>
2022-11-22 14:52:39 +0700

    tugas loop for dan while

---
+++ loopw.js
@@ -0,0 +1,12 @@
+let i = 0;
+while (i<10){
+    i++;
+        if(i%2==1) {
+            console.log(i , "adalah bilangan ganjil");
+    }
+}
+
+for(var j = 1 ; j<=10;j++){
+    if(j%2===1){
+        console.log(j ,"adalah bilangan ganjil");
+    }}
